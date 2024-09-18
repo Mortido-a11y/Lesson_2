11ee85cb826d5c51ee2cde0a5958740ce839deec
Denis Zenkov <id90814@gmail.com>
2024-09-18 12:43:49 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+homework_count = '12'
+hours_count = '1.5'
+name_of_course = 'Python'
+time_on_task = float(hours_count) / int(homework_count)
+print(name_of_course,',', 'всего задач:',homework_count,',','затрачено часов:',hours_count,',','среднее время выполнения:',time_on_task)
